7ad46fbc1b215650ae19d43236b3c25984ca5fd3
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2024-07-18 21:48:17 -0700

    add: temporal pca function boilerplate

---
+++ R/pca.R
@@ -0,0 +1,14 @@
+#' The length of a string
+#'
+#' todo: description goes here...
+#'
+#' @param eyeris An object of class `eyeris` dervived from [eyeris::load()].
+#'
+#' @return A numeric vector giving number of characters (code points) in each
+#'    element of the character vector. Missing string have missing length.
+#' @export
+#' @examples
+#' str_length(letters)
+pca <- function(eyeris) {
+  ## TODO
+}
