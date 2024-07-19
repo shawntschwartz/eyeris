87a9713fdc8561fd965b85b617e8e5e4d8b08aee
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2024-07-18 21:44:48 -0700

    add: pupil despeed function

---
+++ R/despeed.R
@@ -0,0 +1,38 @@
+#' The length of a string
+#'
+#' todo: description goes here...
+#'
+#' @param eyeris An object of class `eyeris` dervived from [eyeris::load()].
+#'
+#' @return A numeric vector giving number of characters (code points) in each
+#'    element of the character vector. Missing string have missing length.
+#' @export
+#' @examples
+#' str_length(letters)
+despeed <- function(eyeris, n = 16) {
+  return(pipeline_handler(eyeris, despeed.pupil, 'despeed', n))
+}
+
+# based on https://github.com/dr-JT/pupillometry/blob/main/R/pupil_artifact.R
+despeed.pupil <- function(x, prev_op, n) {
+  pupil <- x[[prev_op]]
+  time <- x[['time_orig']]
+
+  pupil_speed <- speed(pupil, time)
+  median_speed <- median(pupil_speed, na.rm = TRUE)
+  mad_val <- median(abs(pupil_speed - median_speed), na.rm = TRUE)
+  mad_thresh <- median_speed + (n * mad_val)
+  pupil <- ifelse(pupil_speed >= mad_thresh, as.numeric(NA), pupil)
+
+  return(pupil)
+}
+
+speed <- function(x, y) {
+  delta <- diff(x) / diff(y)
+
+  pupil <- abs(cbind(c(NA, delta), c(delta, NA)))
+  pupil <- apply(pupil, 1, max, na.rm = TRUE)
+  pupil <- ifelse(pupil == -Inf, NA, pupil)
+
+  return(pupil)
+}
