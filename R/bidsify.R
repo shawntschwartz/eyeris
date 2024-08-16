67ed86a6f916fe6a6f349585c2e41dcde380d340
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2024-08-16 00:46:54 -0700

    feat: overhual functionality of bidsify.R for efficiency and to resolve edge cases

--- R/bidsify.R
+++ R/bidsify.R
@@ -9,34 +9,41 @@
 #' @export
 #' @examples
 #' str_length(letters)
-bidsify <- function(eyeris, epochs = 'all', merge = TRUE, save = TRUE, dir = NULL, sub = NULL, ses = NULL, task = NULL, run = NULL) {
+bidsify <- function(eyeris, save.all = TRUE, epochs.list = NULL, merge.epochs = FALSE,
+                    bids.dir = NULL, bids.subid = NULL, bids.sessionid = NULL,
+                    bids.taskid = NULL, bids.runid = NULL, save.raw = TRUE) {
+
+  sub <- bids.subid
+  ses <- bids.sessionid
+  task <- bids.taskid
+  run <- bids.runid
+  dir <- bids.dir
+
   tryCatch({
     check_data(eyeris, 'bidsify')
   }, error = function(e) {
     error_handler(e, 'input_data_type_error')
   })
 
-  if (save) {
-    tryCatch({
-      check_input(arg = sub)
-    }, error = function(e) {
-      error_handler(e, 'bidsify_input_arg_missing_error')
-    })
-  
-    tryCatch({
-      check_input(arg = task)
-    }, error = function(e) {
-      error_handler(e, 'bidsify_input_arg_missing_error')
-    })
-  
-    tryCatch({
-      check_input(arg = run)
-    }, error = function(e) {
-      error_handler(e, 'bidsify_input_arg_missing_error')
-    })
-  }
-  
-  epochs <- find_epochs(eyeris, epochs)
+  tryCatch({
+    check_input(arg = sub)
+  }, error = function(e) {
+    error_handler(e, 'input_arg_missing_error')
+  })
+
+  tryCatch({
+    check_input(arg = task)
+  }, error = function(e) {
+    error_handler(e, 'input_arg_missing_error')
+  })
+
+  tryCatch({
+    check_input(arg = run)
+  }, error = function(e) {
+    error_handler(e, 'input_arg_missing_error')
+  })
+
+  epochs <- filter_epochs(eyeris, epochs.list)
 
   tryCatch({
     count_epochs(epochs)
@@ -44,44 +51,74 @@ bidsify <- function(eyeris, epochs = 'all', merge = TRUE, save = TRUE, dir = NUL
     error_handler(e, 'epoch_count_error')
   })
 
-  x <- list()
-
-  if (merge) {
-    x[[1]] <- epochs_to_single_tibble(epochs)
+  if (save.all) {
+    epochs_to_save <- eyeris[epochs]
+  } else if (!is.null(epochs.list)) {
+    epochs_to_save <- eyeris[epochs.list]
   } else {
-    x <- epochs
+    stop('Either save.all must be TRUE or epochs.list must be specified.')
   }
-  
-  if (save) {
-    check_and_create_dir(dir)
 
-    p <- file.path('derivatives')
+  check_and_create_dir(dir)
+  p <- file.path('derivatives')
+
+  check_and_create_dir(dir, p)
+
+  if (!is.null(sub)) {
+    p <- file.path(p, paste0('sub-', sub))
     check_and_create_dir(dir, p)
-    
-    if (!is.null(sub)) {
-      p <- file.path(p, paste0('sub-', sub))
-      check_and_create_dir(dir, p)
-    }
+  }
 
-    if (!is.null(ses)) {
-      p <- file.path(p, paste0('ses-', ses))
-      check_and_create_dir(dir, p)
-    }
-
-    p <- file.path(p, 'pupil')
+  if (!is.null(ses)) {
+    p <- file.path(p, paste0('ses-', ses))
     check_and_create_dir(dir, p)
+  }
 
-    if (!is.null(ses)) {
-      fnames <- paste0('sub-', sub, '_ses-', ses, '_task-', task, '_run-', run, '_epoch-', epochs, '_desc-preproc_pupil.csv')
-    } else {
-      fnames <- paste0('sub-', sub, '_task-', task, '_run-', run, '_epoch-', epochs, '_desc-preproc_pupil.csv')
-    }
+  p <- file.path(p, 'pupil')
+  check_and_create_dir(dir, p)
 
-    for (i in seq_along(epochs)) {
-      cli::cli_alert_info(sprintf("Writing epoched data to '%s'...", file.path(dir, p, fnames[i])))
-      write.csv(x[[i]], file.path(dir, p, fnames[i]), row.names = FALSE)
-      cli::cli_alert_success(sprintf("Epoched data successfully writteen to: '%s'", file.path(dir, p, fnames[i])))
-    }
-    
+  if (!merge.epochs) {
+    lapply(names(epochs_to_save), function(epoch_id) {
+      current_label <- substr(epoch_id, 7, nchar(epoch_id))
+
+      f <- make_bids_fname(sub = sub, ses = ses, task = task, run = run,
+                           epoch = current_label, desc = 'preproc_pupil')
+
+      alert('info', "Writing epoched data to '%s'...", file.path(dir, p, f))
+
+      write.csv(epochs_to_save[[epoch_id]],
+                file = file.path(bids.dir, p, f),
+                row.names = FALSE)
+
+      alert('success', "Epoched data successfully written to: '%s'",
+            file.path(dir, p, f))
+    })
+  } else {
+    f <- make_bids_fname(sub = sub, ses = ses, task = task, run = run,
+                         epoch = 'all', desc = 'preproc_pupil')
+
+    alert('info', "Writing merged epoched data to '%s'...", file.path(dir, p, f))
+
+    merged_epochs <- do.call(rbind, epochs_to_save)
+
+    write.csv(merged_epochs,
+              file = file.path(bids.dir, p, f),
+              row.names = FALSE)
+
+    alert('success', "Merged epoched data successfully written to: '%s'",
+          file.path(dir, p, f))
+  }
+
+  if (save.raw) {
+    f <- make_bids_fname(sub = sub, ses = ses, task = task, run = run,
+                         desc = 'timeseries_pupil')
+
+    alert('info', "Writing raw pupil timeseries data to '%s'...",
+          file.path(dir, p, f))
+
+    write.csv(eyeris$timeseries, file.path(dir, p, f), row.names = FALSE)
+
+    alert('success', "Raw pupil timeseries data successfully written to: '%s'",
+          file.path(dir, p, f))
   }
 }
