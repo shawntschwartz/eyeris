561a97070e0f9f7304a7518b079f58b2f5211fd0
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2024-08-16 00:52:43 -0700

    update: example.R script

--- example.R
+++ example.R
@@ -8,14 +8,21 @@ el.preproc <- el.raw |>
   # eyeris::deblink(extend = 75) |>
   # eyeris::despeed(n = 8) |>
   # eyeris::butterfilt(type = 'low', order = 4, freq = 4)
-  eyeris::butterfilt(type = 'low', order = 4, freq = 0.5)
+  eyeris::butterfilt(type = 'low', order = 4, freq = 0.5) |>
   # eyeris::deblink(extend = 75) |>
   # eyeris::despeed(n = 8) |>
   # eyeris::smooth(n = 250) #|>
   # eyeris::detrend() |>
   # eyeris::zscore() |>
-  # eyeris::epoch(event.marker = 'ENCGOAL_',
-  #               dur.secs = 4,
-  #               matching.type = 'boundary',
-  #               metadata.template = 'trial')
+  eyeris::epoch(event.marker = 'ENCGOAL_',
+                dur.secs = 4,
+                matching.type = 'boundary',
+                metadata.template = 'trial')
 
+el.preproc |>
+  eyeris::bidsify(bids.dir = '~/Documents',
+                  bids.subid = '001',
+                  bids.sessionid = '01',
+                  bids.taskid = 'madore20',
+                  bids.runid = '01',
+                  save.raw = TRUE)
