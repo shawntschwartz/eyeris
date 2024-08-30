5f09ad3e0c444e8d53b519e121fd81c010741b50
shawn <s@shawnts.com>
2024-08-30 10:48:44 -0700

    update: `detrend()` docs #21

--- R/detrend.R
+++ R/detrend.R
@@ -1,14 +1,17 @@
-#' The length of a string
-#'
+#' Detrend the pupil time series
+#' 
 #' todo: description goes here...
 #'
 #' @param eyeris An object of class `eyeris` dervived from [eyeris::load()].
 #'
 #' @return A numeric vector giving number of characters (code points) in each
 #'    element of the character vector. Missing string have missing length.
-#' @export
+#' 
 #' @examples
-#' str_length(letters)
+#' eyeris_data |>
+#'   eyeris::detrend()
+#' 
+#' @export
 detrend <- function(eyeris) {
   return(pipeline_handler(eyeris, detrend.pupil, 'detrend'))
 }
