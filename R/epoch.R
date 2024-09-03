1cd4474cabb889d1a044889692a1a6cc67baf005
shawn <s@shawnts.com>
2024-09-02 23:57:43 -0700

    resolve #22: complete `epoch()` docs

--- R/epoch.R
+++ R/epoch.R
@@ -1,26 +1,42 @@
 #' Epoch pupil data based on custom event message structure
-#' 
-#' Intended to be used as the last step of preprocessing.This function creates data epochs
-#' of fixed duration with respect to event.markers (str). 
-#' Also remove characters in the event messages that are not alphanumeric or space, 
-#' and convert the message to a camelCase format.
-#' 
-#' @param eyeris An object of class `eyeris` derived from [eyeris::load()].
-#' @param event.marker A single string representing the event marker based on which to epoch data
-#' @param dur.secs Duration of epochs in seconds.
-#' @param matching.type Regular expression method to match provided event maker string with events in the data. Use 'boundary' for exact match, and 'contains' for partial match.
-#' @param hz Data sampling rate. If not specified, will use the value in metadata.
-#' @param metadata.template A space-separated string as a template for separating the full event message into parts. The event.marker sting in the event message in the data will be removed, and the remaining string will be separated based on space. These element strings will be put in separate columns of the output with column names being the template.
 #'
-#' @return Updated `eyeris` object with epoched data.
-#' 
+#' Intended to be used as the final preprocessing step.
+#' This function creates data epochs of fixed duration with respect to
+#' your provided `event.markers` (str), and also sanitizes (i.e., remove
+#' characters from the provided event messages that are not alphanumeric
+#' (or spaces) and convert the message to a camelCase format.
+#'
+#' @param eyeris An object of class `eyeris` derived from [eyeris::load()].
+#' @param event.marker A single string representing the event marker based on
+#' when to epoch the continuous pupil timeseries data.
+#' @param dur.secs Duration of epochs (in seconds).
+#' @param matching.type Indicates which regular expression method will be used
+#' to perform pattern matching with the provided event maker string and the
+#' events messages contained within the raw data.
+#' Use 'boundary' for exact matching, and 'contains' for partial matching.
+#' @param hz Data sampling rate. If not specified, will use the value contained
+#' within the tracker's metadata.
+#' @param metadata.template A space-separated string as a template for parsing
+#' the full event message into parts. The event.marker string that is matched
+#' with the event messages contained in the raw pupil data will be removed, and
+#' the remaining string will be separated based on where space(s) occur within
+#' the event.marker string. These element strings will be placed into separate
+#' columns of the epoched outputs (i.e., dataframes within the `eyeris` object
+#' that begin with `epoch_`), where rows contain the specific text matched
+#' to the indices of the strings provided in the template string (and the string
+#' from each index of the template string becomes the column name(s) containing
+#' these metadata values in the epoched data).
+#'
+#' @return Updated `eyeris` object with dataframes containing the epoched data
+#' (`epoch_`).
+#'
 #' @examples
 #' eyeris_data |>
 #'   eyeris::epoch(event.marker = "ITI",
 #'                 dur.secs = 3,
 #'                 matching.type = "boundary",
 #'                 metadata.template = "trial stim")
-#' 
+#'
 #' @export
 epoch <- function(eyeris, event.marker, dur.secs, matching.type = c('boundary', 'contains'), hz = NULL, metadata.template = NULL) {
   return(pipeline_handler(eyeris, epoch.pupil, 'epoch', event.marker, dur.secs, matching.type, hz, metadata.template))
