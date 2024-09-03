5a9e103b292b01fe141c83d03bf726c81c8f9a04
shawn <s@shawnts.com>
2024-09-02 23:56:16 -0700

    update: `baseline()` docs #16

--- R/baseline.R
+++ R/baseline.R
@@ -1,11 +1,19 @@
 #' Subtractive / divisive baselining of pupil data
-#' 
-#' This function is intended for event-related analyses where one would like to control for different pupil size before the event of interest. This may be a suitable replacement of linear detrend in some cases, such as when one still wants to analyze the baseline pupil, or when baseline pupil changes in a severly nonlinear fashion.
+#'
+#' This function is intended for event-related analyses where one would like to
+#' control for varying pupil size before an event of interest. This may be a
+#' suitable replacement for linear detrending [eyeris::detrend()] in some cases,
+#' such as when one still wants to analyze the baseline pupil size, or when
+#' the baseline pupil size changes in a severly nonlinear fashion.
 #'
 #' @param eyeris An object of class `eyeris` dervived from [eyeris::load()].
 #'
-#' @return Updated `eyeris` object with baseline removed.
-#' 
+#' @return An `eyeris` object with a new dataframe containing the pupil
+#' timeseries after baseline removal.
+#'
+#' @examples
+#' eyeris_data |>
+#'  eyeris::baseline()
 baseline <- function(eyeris) {
   ## TODO
 }
