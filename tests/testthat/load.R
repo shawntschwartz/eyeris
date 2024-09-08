f53881d3892aa0c0efdc72f553c4658679f6d9e8
shawn schwartz <s@shawnts.com>
2024-09-08 10:42:37 -0700

    tests: add `load.R` unit test for expected objects in loaded eyeris object

---
+++ tests/testthat/load.R
@@ -0,0 +1,10 @@
+testthat::test_that("load function returns list with expected objects", {
+  eye_file <- system.file("extdata", "assocret.asc", package = "eyeris")
+  result <- eyeris::load(eye_file)
+
+  expected_objects <- c("file", "timeseries", "events", "blinks", "info", "latest")
+  actual_objects <- names(result)
+
+  testthat::expect_true(all(expected_objects %in% actual_objects),
+                        info = "Not all expected objects in `eyeris` object are present in the loaded data.")
+})
