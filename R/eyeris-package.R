5899cfabfc53c0309f56518106dacb54ccc0b79c
shawn schwartz <s@shawnts.com>
2024-09-09 15:44:36 -0700

    build: resolve #40

---
+++ R/eyeris-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr across
+#' @importFrom dplyr all_of
+#' @importFrom graphics text
+#' @importFrom rlang :=
+#' @importFrom stats lm
+#' @importFrom stats median
+#' @importFrom stats sd
+#' @importFrom utils write.csv
+## usethis namespace: end
+NULL
---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+utils::globalVariables(c(
+  "time_orig", "pupil", "blink", "blink.lag", "time", "blink.start",
+  "blink.lead", "blink.end", "pupil_deblink", "event", "ps", "xp", "yp",
+  "pupil_raw", "type", "block", "timebin", "step", "eyeris", "pupil_size"
+))
