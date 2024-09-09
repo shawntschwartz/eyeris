03586d6c1fcbfdad9b59820a74560897c77041b4
shawn schwartz <s@shawnts.com>
2024-09-09 15:55:03 -0700

    lint: `detrend.R` and `detrend.Rd` #45

--- R/detrend.R
+++ R/detrend.R
@@ -1,30 +1,33 @@
 #' Detrend the pupil time series
 #'
-#' Linearly detrend pupil data by fitting a linear model of `pupil_data ~ time`,
+#' Linearly detrend_pupil data by fitting a linear model of `pupil_data ~ time`,
 #' and return the residuals (`pupil_data - fitted_values`).
 #'
 #' @param eyeris An object of class `eyeris` dervived from [eyeris::load()].
 #'
-#' @return An `eyeris` object with a new column in `timeseries`: `pupil_detrend`.
+#' @return An `eyeris` object with a new column in `timeseries`:
+#' `pupil_detrend`.
 #'
 #' @examples
+#' \dontrun{
 #' system.file("extdata", "assocret.asc", package = "eyeris") |>
 #'   eyeris::load() |>
-#'   eyeris::deblink(extend = 50) |>  # Bleed around blink periods just long enough to remove majority of deflections due to eyelid movements
+#'   eyeris::deblink(extend = 50) |>
 #'   eyeris::despeed() |>
 #'   eyeris::interpolate() |>
 #'   eyeris::detrend()
+#' }
 #'
 #' @export
 detrend <- function(eyeris) {
-  return(pipeline_handler(eyeris, detrend.pupil, 'detrend'))
+  return(pipeline_handler(eyeris, detrend_pupil, "detrend"))
 }
 
-detrend.pupil <- function(x, prev_op) {
+detrend_pupil <- function(x, prev_op) {
   pupil <- x[[prev_op]]
-  time <- x[['time_orig']]
+  timeseries <- x[["time_orig"]]
 
-  fit <- lm(pupil ~ time)
+  fit <- lm(pupil ~ timeseries)
   trend <- fit$fitted.values
 
   return(pupil - trend)
