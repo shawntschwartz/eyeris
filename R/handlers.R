7e4b04fe3bc5da9917ddcb4cb777518edbcf909f
shawn schwartz <s@shawnts.com>
2024-09-09 15:56:21 -0700

    lint: `handlers.R` #45

--- R/handlers.R
+++ R/handlers.R
@@ -8,16 +8,19 @@ error_handler <- function(e, e_class) {
 
 # generic handler/wrapper for eyeris pupil pipeline funcs
 pipeline_handler <- function(eyeris, operation, new_suffix, ...) {
-  tryCatch({
-    check_data(eyeris, new_suffix)
-  }, error = function(e) {
-    error_handler(e, 'input_data_type_error')
-  })
+  tryCatch(
+    {
+      check_data(eyeris, new_suffix)
+    },
+    error = function(e) {
+      error_handler(e, "input_data_type_error")
+    }
+  )
 
   # getters
   prev_operation <- eyeris$latest
 
-  if (new_suffix == 'epoch') {
+  if (new_suffix == "epoch") {
     # run op
     data <- operation(eyeris, prev_operation, ...)
 
@@ -27,7 +30,7 @@ pipeline_handler <- function(eyeris, operation, new_suffix, ...) {
     data <- eyeris$timeseries
 
     # setters
-    output_col <- paste0(prev_operation, '_', new_suffix)
+    output_col <- paste0(prev_operation, "_", new_suffix)
 
     # run op
     data[[output_col]] <- operation(data, prev_operation, ...)
