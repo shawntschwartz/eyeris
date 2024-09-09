72f27fa1587d9b40b167c8450880d96c87db6b51
shawn schwartz <s@shawnts.com>
2024-09-09 15:54:26 -0700

    lint: `interpolate.R` and `interpolate.Rd` #45

--- R/interpolate.R
+++ R/interpolate.R
@@ -1,25 +1,28 @@
 #' Interpolate missing pupil samples
-#' 
+#'
 #' Linear interpolation of time series data. The intended use of this method
 #' is for filling in missing pupil samples (NAs) in the time series.
 #'
 #' @param eyeris An object of class `eyeris` dervived from [eyeris::load()].
 #'
-#' @return An `eyeris` object with a new column in `timeseries`: `pupil_interpolate`.
+#' @return An `eyeris` object with a new column in `timeseries`:
+#' `pupil_interpolate`.
 #'
 #' @examples
+#' \dontrun{
 #' system.file("extdata", "assocret.asc", package = "eyeris") |>
 #'   eyeris::load() |>
-#'   eyeris::deblink(extend = 50) |>  # Bleed around blink periods just long enough to remove majority of deflections due to eyelid movements
+#'   eyeris::deblink(extend = 50) |>
 #'   eyeris::despeed() |>
 #'   eyeris::interpolate()
-#' 
+#' }
+#'
 #' @export
 interpolate <- function(eyeris) {
-  return(pipeline_handler(eyeris, interpolate.pupil, "interpolate"))
+  return(pipeline_handler(eyeris, interpolate_pupil, "interpolate"))
 }
 
-interpolate.pupil <- function(x, prev_op) {
+interpolate_pupil <- function(x, prev_op) {
   if (!any(is.na(x[[prev_op]]))) {
     cli::cli_abort("No NAs detected in pupil data for interpolation.")
   } else {
