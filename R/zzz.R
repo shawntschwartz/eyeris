6395662813881d08edfbd65dc74cb04cc758d5b8
shawn <s@shawnts.com>
2024-09-19 02:07:45 -0700

    update: global vars declarations in `zzz.R`

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 utils::globalVariables(c(
   "time_orig", "pupil", "blink", "blink.lag", "time", "blink.start",
   "blink.lead", "blink.end", "pupil_deblink", "event", "ps", "xp", "yp",
-  "pupil_raw", "type", "block", "timebin", "step", "eyeris", "pupil_size"
+  "pupil_raw", "type", "block", "timebin", "step", "eyeris", "pupil_size",
+  "matched_event", "matching_pattern"
 ))
